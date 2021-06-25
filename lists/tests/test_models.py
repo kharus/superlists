72760b15b59802dcd4f674a6a48a8dc00a453436
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2021-06-25 11:14:41 +0800

    Chapter 13

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,9 +1,11 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from lists.models import Item, List
 
 
 class ListAndItemModelsTest(TestCase):
+
     def test_saving_and_retrieving_items(self):
         list_ = List()
         list_.save()
@@ -30,3 +32,16 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
